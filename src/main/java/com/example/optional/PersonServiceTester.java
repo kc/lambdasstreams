d4a933b1c3af269271214607576f682d8cdb7edc
Bram Janssens <s.a.janssens@gmail.com>
2019-07-09 16:31:37 +0200

    Optional stuff updated.

--- src/main/java/com/example/optional/PersonServiceTester.java
+++ src/main/java/com/example/optional/PersonServiceTester.java
@@ -3,19 +3,21 @@ package com.example.optional;
 public class PersonServiceTester {
 
     public static void main(String[] args) {
+        String name = "Bram";
+
         Person p1 = null;
         Person p2 = new Person();
-        Person p3 = new Person("Bram");
+        Person p3 = new Person(name);
 
         PersonService personService = new PersonService();
 
-        personService.raiseSalaryOldStyle(p1, "Bram");
-        personService.raiseSalaryOldStyle(p2, "Bram");
-        personService.raiseSalaryOldStyle(p3, "Bram");
+        personService.raiseSalaryOldStyle(p1, name);
+        personService.raiseSalaryOldStyle(p2, name);
+        personService.raiseSalaryOldStyle(p3, name + "Foo");
 
-        personService.raiseSalary(p1, "Bram");
-        personService.raiseSalary(p2, "Bram");
-        personService.raiseSalary(p3, "Bram");
+        personService.raiseSalary(p1, name);
+        personService.raiseSalary(p2, name);
+        personService.raiseSalary(p3, name);
 
         System.out.println(p1);
         System.out.println(p2);
