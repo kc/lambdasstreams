c86d1b1e7df450b0f1da6630be64af273bf35272
bramjanssens <bram.janssens@infosupport.com>
2019-07-10 08:19:06 +0200

    Comment modified.

--- src/main/java/com/example/streams/collect/CollectPerson.java
+++ src/main/java/com/example/streams/collect/CollectPerson.java
@@ -14,7 +14,7 @@ public class CollectPerson {
                 Collector.of(
                         () -> new StringJoiner(" | "),  // supplier: creert een muteerbare container voor de (tussen)resultaten
                         (j, p) -> j.add(p.name.toUpperCase()),  // accumulator: voegt het volgende element toe aan de tot nu toe gevulde container en geeft de gevulde container terug
-                        (j1, j2) -> j1.merge(j2),               // combiner: combineert twee containers; hier: voegt twee containers met strings samen met daartussen de delimiter
+                        (j1, j2) -> j1.merge(j2),               // combiner: combineert twee containers die ontstaan bij parallel processing
                         j -> j.toString());                     // finisher: converteert de totaal gevulde container tot één waarde
 
         return persons.stream().collect(personNameCollector);
