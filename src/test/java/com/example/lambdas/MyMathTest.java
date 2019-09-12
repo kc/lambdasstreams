2ab1a5353d52389ee0e8bb5782e187fc20c8776d
bramjanssens <bram.janssens@infosupport.com>
2019-09-12 10:46:12 +0200

    Small improvements.

--- src/test/java/com/example/lambdas/MyMathTest.java
+++ src/test/java/com/example/lambdas/MyMathTest.java
@@ -16,14 +16,15 @@ public class MyMathTest {
     @Test
     public void test() {
         // als een lambda simpelweg delegeert naar een andere methode, kun je in enkele
-        // gevallen direct een referentie naar die methode meegeven ipv de lambda die hem aanroept
+        // gevallen direct een referentie naar die andere methode meegeven ipv de lambda die hem aanroept
 
         MyMath myMath = new MyMath(42);
 
         // a method reference to a static method
         int execute1 = execute(4, (int a) -> a + 1);
-        int execute2 = execute(4, (int a) -> MyMath.x(a)); // apply x on 4
+        int execute2 = execute(4, (int a) -> MyMath.x(a)); // put 4 into function x
         int execute3 = execute(4, MyMath::x);
+
         assertThat(execute1, is(execute2));
         assertThat(execute2, is(execute3));
 
@@ -44,15 +45,11 @@ public class MyMathTest {
 
         // constructor reference
         IntFunction<MyMath> f = MyMath::new;
-
-        int value = 100;
-        MyMath apply = f.apply(value);
-        assertThat(apply.i, is(value));
+        MyMath apply = f.apply(100);
+        assertThat(apply.i, is(100));
     }
 
-    private int execute(int i, IntFunction<Integer> c) {
-        return c.apply(i);
-    }
+    private int execute(int i, IntFunction<Integer> c) { return c.apply(i); }
 
     private void execute(MyMath m, int i, BiConsumer<MyMath, Integer> c) {
         c.accept(m, i);
@@ -62,11 +59,5 @@ public class MyMathTest {
         return stringFunction.apply(s, skip);
     }
 
-    private <T, U> T execute(T instance, U arg, BiFunction<T, U, T> f) {
-        return f.apply(instance, arg);
-    }
-
-    private void execute(int i, IntConsumer c) {
-        c.accept(i);
-    }
+    private void execute(int i, IntConsumer c) { c.accept(i); }
 }
