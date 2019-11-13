fcade734433fff88e634cd3c4bda187e388d0864
bramjanssens <bram.janssens@infosupport.com>
2019-11-13 09:43:04 +0100

    Small improvement.

--- src/test/java/com/example/lambdas/functionalinterfaces/UsingConsumerTest.java
+++ src/test/java/com/example/lambdas/functionalinterfaces/UsingConsumerTest.java
@@ -2,6 +2,7 @@ package com.example.lambdas.functionalinterfaces;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,11 +10,18 @@ import static com.example.lambdas.functionalinterfaces.UsingConsumer.forEach;
 
 public class UsingConsumerTest {
 
+    private final List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
     @Test
     public void printIntegers() {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-        forEach(list, (Integer i) -> System.out.println(i));
-        forEach(list, s -> list.add(s)); // void compatibility: we can pass in a Predicate (lambda returns boolean) as a Consumer! (Boolean) Statement expression is compatible with void.
+        forEach(list, System.out::println);
+    }
+
+    @Test
+    public void addIntegers() {
+        List<Integer> copy = new ArrayList<>();
+        forEach(list, s -> copy.add(s)); // void compatibility: we can pass in a Predicate (lambda returns boolean) as a Consumer! (Boolean) Statement expression is compatible with void.
+        forEach(copy, System.out::println);
     }
 
 }
