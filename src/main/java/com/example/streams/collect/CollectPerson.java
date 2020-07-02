e1e31fbb2e6491ff72f944b58485de02ec1d62a8
bramjanssens <bram.janssens@infosupport.com>
2020-07-02 16:11:23 +0200

    Some comments.

--- src/main/java/com/example/streams/collect/CollectPerson.java
+++ src/main/java/com/example/streams/collect/CollectPerson.java
@@ -2,19 +2,22 @@ package com.example.streams.collect;
 
 import com.example.streams.Person;
 
-import java.util.List;
 import java.util.StringJoiner;
 import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import static com.example.streams.ExampleCollections.persons;
 
+/**
+ * Demo of:
+ * - Collectors, through a custom Collector
+ * - collect vs reduce.
+ */
 public class CollectPerson {
 
     public String collectPersonNames() {
         Collector<Person, StringJoiner, String> personNameCollector =
                 Collector.of(
-                        () -> new StringJoiner(" | "),  // supplier: creert een muteerbare container voor de (tussen)resultaten
+                        () -> new StringJoiner(" | "),          // supplier: creert een muteerbare container voor de (tussen)resultaten
                         (j, p) -> j.add(p.name.toUpperCase()),  // accumulator: voegt het volgende element toe aan de tot nu toe gevulde container en geeft de gevulde container terug
                         (j1, j2) -> j1.merge(j2),               // combiner: combineert twee containers die ontstaan bij parallel processing
                         j -> j.toString());                     // finisher: converteert de totaal gevulde container tot één waarde
@@ -23,8 +26,6 @@ public class CollectPerson {
     }
 
     public String reducePersonNames() {
-        Collector<Object, ?, List<Object>> objectListCollector = Collectors.toList();
-
         return persons.stream()
                 .map(p -> p.name)
                 .map(String::toUpperCase)
