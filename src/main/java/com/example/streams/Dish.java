73f638b32265d6b65ab8c9ec2a8ab4df5aea69cb
bramjanssens <bram.janssens@infosupport.com>
2021-01-06 08:22:06 +0100

    DishTest added.

--- src/main/java/com/example/streams/Dish.java
+++ src/main/java/com/example/streams/Dish.java
@@ -7,16 +7,13 @@ public class Dish {
     private final int calories;
     private final Dish.Type type;
 
+    public enum Type {MEAT, FISH, OTHER}
+
     public Dish(String name, boolean vegetarian, int calories, Dish.Type type) {
         this.name = name;
         this.vegetarian = vegetarian;
         this.calories = calories;
         this.type = type;
-    }
-
-
-    public enum Type {
-        MEAT, FISH, OTHER;
     }
 
     public String getName() {
@@ -33,5 +30,15 @@ public class Dish {
 
     public Type getType() {
         return type;
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "name='" + name + '\'' +
+                ", vegetarian=" + vegetarian +
+                ", calories=" + calories +
+                ", type=" + type +
+                '}';
     }
 }
---
+++ src/test/java/com/example/streams/DishTest.java
@@ -0,0 +1,18 @@
+package com.example.streams;
+
+import org.junit.Test;
+
+public class DishTest {
+
+    @Test
+    public void lowCaloricClassicStyle() {
+        // TODO Lab 3.1.1
+        // result should be [prawns, rice, season fruit]
+    }
+
+    @Test
+    public void lowCaloricStreams() {
+        // TODO Lab 3.1.2
+        // result should be [prawns, rice, season fruit]
+    }
+}
